d86f0e5710d4c9825da3158cca234e3ac5c189ad
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-03 15:45:50 +0200

    mv from vidly

--- index.js
+++ index.js
@@ -1,102 +1,102 @@
+const helmet = require('helmet');
 const Joi = require('@hapi/joi');
+const logger = require('./logger');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
 
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' }
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
+
+app.use(logger);
+
+const genres = [
+  { id: 1, name: 'Drama' },
+  { id: 2, name: 'Comedy' },
+  { id: 3, name: 'Thriller' },
+  { id: 4, name: 'Horror' }
 ];
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile('index.html', { root: __dirname });
 });
 
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
 
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found.');
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+
+  if (!genre) {
+    return res.status(404).send('The genre with the given ID was not found.');
   } else {
-    return res.send(course);
+    return res.send(genre);
   }
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.params);
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body); // result.error
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body); // result.error
 
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const course = {
-    id: courses.length + 1,
+  const genre = {
+    id: genres.length + 1,
     name: req.body.name
   };
 
-  courses.push(course);
-  res.send(course);
+  genres.push(genre);
+  res.send(genre);
 });
 
-app.put('/api/courses/:id', (req, res) => {
-  // Look up the course
-  const course = courses.find(c => c.id === parseInt(req.params.id));
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
 
-  // If not existing, return 404
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found.');
+  if (!genre) {
+    return res.status(404).send('The genre with the given ID was not found.');
   }
 
-  // Validate
-  const { error } = validateCourse(req.body); // result.error
+  const { error } = validateGenre(req.body); // result.error
 
-  // If invalid, return 400 - Bad Request
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  // Update course
-  course.name = req.body.name;
+  genre.name = req.body.name;
 
-  // Return the updated course
-  res.send(course);
+  res.send(genre);
 });
 
-app.delete('/api/courses/:id', (req, res) => {
-  // Look up the course
-  const course = courses.find(c => c.id === parseInt(req.params.id));
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
 
-  // If not existing, return 404
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found.');
+  if (!genre) {
+    return res.status(404).send('The genre with the given ID was not found.');
   }
 
-  // Delete
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
 
-  // Return the same course
-  res.send(course);
+  res.send(genre);
 });
 
-function validateCourse(course) {
+function validateGenre(genre) {
   const schema = {
     name: Joi.string()
       .min(3)
       .required()
   };
 
-  return Joi.validate(course, schema);
+  return Joi.validate(genre, schema);
 }
 
 const port = process.env.PORT || 3000;
