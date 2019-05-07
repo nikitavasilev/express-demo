9dfb85002f788997f6e489f47644ce2d99164eaa
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-07 02:52:48 +0200

    restructuring

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const dbDebugger = require('debug')('app:db');
 // const debug = require('debug')('app:startup'):
 const config = require('config');
 const helmet = require('helmet');
-const Joi = require('@hapi/joi');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+const genres = require('./routes/genres');
+const home = require('./routes/home');
+const about = require('./routes/about');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
@@ -17,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+app.use('/api/genres', genres);
+app.use('/', home);
+app.use('/', about);
 
 // Configuration
 console.log(`Application name: ${config.get('name')}`);
@@ -32,92 +37,6 @@ if (app.get('env') === 'development') {
 dbDebugger('Connected to the database...');
 
 app.use(logger);
-
-const genres = [
-  { id: 1, name: 'Drama' },
-  { id: 2, name: 'Comedy' },
-  { id: 3, name: 'Thriller' },
-  { id: 4, name: 'Horror' }
-];
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'My about page', message: 'Hello' });
-});
-
-app.get('/api/genres', (req, res) => {
-  res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-  if (!genre) {
-    return res.status(404).send('The genre with the given ID was not found.');
-  } else {
-    return res.send(genre);
-  }
-});
-
-app.post('/api/genres', (req, res) => {
-  const { error } = validateGenre(req.body); // result.error
-
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-  if (!genre) {
-    return res.status(404).send('The genre with the given ID was not found.');
-  }
-
-  const { error } = validateGenre(req.body); // result.error
-
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  genre.name = req.body.name;
-
-  res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-  if (!genre) {
-    return res.status(404).send('The genre with the given ID was not found.');
-  }
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 const port = process.env.PORT || 3000;
 
